b7c7b8654350d8ff31af17d3ca24599a18395a3b
EuneeChung <chungee375@gmail.com>
2020-08-03 22:53:59 +0900

    [login] Sign Up View

--- app/src/main/java/com/soundee/soundee/login/LoginActivity.kt
+++ app/src/main/java/com/soundee/soundee/login/LoginActivity.kt
@@ -1,19 +1,27 @@
 package com.soundee.soundee.login
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat.getColor
 import com.soundee.soundee.R
+import com.soundee.soundee.util.CheckTextWatcher
 import kotlinx.android.synthetic.main.actionbar_all_gotoback.*
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        val textWatcher= CheckTextWatcher({checkEmailPassword()})
         txt_actionbar_name.text = "로그인"
 
         edt_login_email.setOnFocusChangeListener() { view, b -> haveFocus(view, b) }
@@ -21,57 +29,61 @@ class LoginActivity : AppCompatActivity() {
 
         edt_login_email.addTextChangedListener(textWatcher)
         edt_login_password.addTextChangedListener(textWatcher)
-    }
 
-    private val textWatcher = object : TextWatcher {
-        override fun afterTextChanged(s: Editable?) {
-        }
-
-        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-        }
-
-        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-            checkEmailPassword()
+        btn_login_signup.setOnClickListener {
+            val intent = Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
         }
     }
 
 
-    private fun haveFocus(view: View, b: Boolean) {
-        if (b) view.setBackgroundResource(R.drawable.border_green_line_4)
-        else view.setBackgroundResource(R.drawable.border_gray_line_4)
-    }
 
-    private fun activeLoginButton(b: Boolean) {
-        if (b) {
-            btn_login_login.setBackgroundResource(R.drawable.border_green_fill_4)
-            btn_login_login.setTextColor(getColor(R.color.colorWhite))
-            btn_login_login.isEnabled = true
-        } else {
-            btn_login_login.setBackgroundResource(R.drawable.border_green_line_4)
-            btn_login_login.setTextColor(getColor(R.color.colorTextBlack))
-            btn_login_login.isEnabled = false
-        }
-    }
 
-    private fun checkEmailPassword() {
-
-        if (android.util.Patterns.EMAIL_ADDRESS.matcher(edt_login_email.text).matches() && edt_login_password.text.toString().isNotEmpty()) {
-            activeLoginButton(true)
-        }
+    private fun checkEmailPassword(){
 
         if (android.util.Patterns.EMAIL_ADDRESS.matcher(edt_login_email.text).matches()) txt_login_check_email.visibility = View.INVISIBLE
-        else {
-            txt_login_check_email.visibility = View.VISIBLE
-            activeLoginButton(false)
-        }
+        else txt_login_check_email.visibility = View.VISIBLE
+
 
         if (edt_login_password.text.isNotEmpty()) txt_login_check_password.visibility = View.INVISIBLE
-        else {
-            txt_login_check_password.visibility = View.VISIBLE
-            activeLoginButton(false)
-        }
+        else txt_login_check_password.visibility = View.VISIBLE
 
+        activeButton(btn_login_login,checkAll(),this)
+
+
+    }
+
+    private fun checkAll():Boolean{
+       if (txt_login_check_email.visibility == View.VISIBLE || edt_login_email.text.isEmpty()) {
+           Log.e("여기","1")
+            edt_login_email.requestFocus()
+           return   false
+        } else if (txt_login_check_password.visibility == View.VISIBLE || edt_login_password.text.isEmpty()) {
+           Log.e("여기","2")
+            edt_login_password.requestFocus()
+           return   false
+        } else {
+           Log.e("여기","3")
+           return  true
+        }
     }
 }
 
+
+fun haveFocus(view: View, b: Boolean) {
+    if (b) view.setBackgroundResource(R.drawable.border_green_line_4)
+    else view.setBackgroundResource(R.drawable.border_gray_line_4)
+}
+
+fun activeButton(view:TextView, b: Boolean,context: Context) {
+    Log.e("여기",b.toString())
+    if (b) {
+        view.setBackgroundResource(R.drawable.border_green_fill_4)
+        view.setTextColor(getColor(context,R.color.colorWhite))
+        view.isEnabled = true
+    } else {
+        view.setBackgroundResource(R.drawable.border_green_line_4)
+        view.setTextColor(getColor(context,R.color.colorTextBlack))
+        view.isEnabled = false
+    }
+}
---
+++ app/src/main/java/com/soundee/soundee/login/SignUpActivity.kt
@@ -0,0 +1,122 @@
+package com.soundee.soundee.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.text.TextWatcher
+
+import android.view.View
+import androidx.core.view.isVisible
+import com.soundee.soundee.R
+import com.soundee.soundee.util.CheckTextWatcher
+import kotlinx.android.synthetic.main.actionbar_all_gotoback.*
+import kotlinx.android.synthetic.main.activity_login.*
+
+import kotlinx.android.synthetic.main.activity_sign_up.*
+import java.util.regex.Pattern
+
+class SignUpActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        txt_actionbar_name.text = "회원가입"
+        txt_signup_check_password_again.visibility = View.INVISIBLE
+
+        edt_signup_email.addTextChangedListener(CheckTextWatcher{checkEmail()})
+        edt_signup_name.addTextChangedListener(CheckTextWatcher{checkNameLength20()})
+        edt_signup_password.addTextChangedListener(CheckTextWatcher{checkPassword()})
+        edt_signup_password_again.addTextChangedListener(CheckTextWatcher{checkPasswordAgain()})
+
+        edt_signup_email.setOnFocusChangeListener(){view,b-> haveFocus(view,b)}
+        edt_signup_name.setOnFocusChangeListener(){view,b-> haveFocus(view,b)}
+        edt_signup_password.setOnFocusChangeListener(){view,b-> haveFocus(view,b)}
+        edt_signup_password_again.setOnFocusChangeListener(){view,b-> haveFocus(view,b)}
+
+        btn_signup_signup.setOnClickListener {
+            moveFocus()
+
+            //통신으로 대체되어야 함.
+            //val intent= Intent(this, LoginActivity::class.java)
+            //startActivity(intent)
+        }
+
+
+    }
+
+    private fun checkEmail() {
+        if (android.util.Patterns.EMAIL_ADDRESS.matcher(edt_signup_email.text).matches()) {
+            txt_signup_check_email.visibility = View.INVISIBLE
+        } else {
+            txt_signup_check_email.visibility = View.VISIBLE
+        }
+        activeButton(btn_signup_signup,checkAll(),this)
+
+    }
+
+    private fun checkNameLength20() {
+        if (edt_signup_name.text.length > 20) {
+            txt_signup_check_name.visibility = View.VISIBLE
+        } else txt_signup_check_name.visibility = View.INVISIBLE
+
+        activeButton(btn_signup_signup,checkAll(),this)
+    }
+
+    private fun checkPassword() {
+
+        if (Pattern.matches(
+                "^.*(?=.{8,20})(?=.*[0-9])(?=.*[a-zA-Z]).*\$",
+                edt_signup_password.text.toString()
+            )
+        ) {
+            txt_signup_check_password.visibility = View.INVISIBLE
+        } else {
+            txt_signup_check_password.visibility = View.VISIBLE
+        }
+        activeButton(btn_signup_signup,checkAll(),this)
+    }
+
+
+    private fun checkPasswordAgain() {
+        if (edt_signup_password.text.toString() == edt_signup_password_again.text.toString()) {
+            txt_signup_check_password_again.visibility = View.INVISIBLE
+        } else {
+            txt_signup_check_password_again.visibility = View.VISIBLE
+            btn_signup_signup.isEnabled = false
+        }
+        activeButton(btn_signup_signup,checkAll(),this)
+    }
+
+    private fun checkAll():Boolean{
+        return if(txt_signup_check_email.visibility==View.VISIBLE){
+            false
+        } else if(txt_signup_check_name.visibility==View.VISIBLE){
+            false
+        } else if(txt_signup_check_password.visibility== View.VISIBLE){
+            false
+        } else if(txt_signup_check_password_again.visibility==View.VISIBLE){
+            false
+        } else {
+            true
+        }
+    }
+    private fun  moveFocus(){
+        if(txt_signup_check_email.visibility==View.VISIBLE || edt_signup_email.text.isEmpty()){
+            edt_signup_email.requestFocus()
+        }
+        else if(txt_signup_check_name.visibility==View.VISIBLE || edt_signup_name.text.isEmpty()){
+            edt_signup_name.requestFocus()
+        }
+        else if(txt_signup_check_password.visibility== View.VISIBLE||edt_signup_password.text.isEmpty()){
+            edt_signup_password.requestFocus()
+        }
+        else if(txt_signup_check_password.visibility==View.VISIBLE|| edt_signup_password_again.text.isEmpty()){
+            edt_signup_password_again.requestFocus()
+        }
+    }
+
+
+
+}
+
